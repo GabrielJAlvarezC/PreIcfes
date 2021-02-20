41981f93a55ee18a46dab758c172e484eea32289
Dapstab <dspt1996@gmail.com>
2021-02-20 10:41:01 -0500

    Ahora se llamaran las funciones respectivas de cada página

--- public/js/index.js
+++ public/js/index.js
@@ -1,38 +1,10 @@
 import "@babel/polyfill";
-import { Login } from "./login";
+import { LoginDOM } from "./homePage/login";
+import { SignupDOM } from "./signupPage/signup";
 
-// Elementos HTML
-const loginForm = document.querySelector(".formLogin");
-const signupForm = document.querySelector(".formSignup");
+// HomePage
+LoginDOM.login();
+LoginDOM.animationInput();
 
-if (loginForm) {
-  loginForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const correo = document.getElementById("email").value;
-    const clave = document.getElementById("password").value;
-    Login.login(correo, clave);
-  });
-}
-
-if (signupForm) {
-  signupForm.addEventListener("submit", (e) => {
-    console.log("hola");
-    e.preventDefault();
-    const nombre = document.getElementById("username").value;
-    const apellido = document.getElementById("lastname").value;
-    const correo = document.getElementById("email").value;
-    const clave = document.getElementById("password").value;
-    const confirmarClave = document.getElementById("passwordConfirm").value;
-    const grado = document.getElementById("grade").value;
-    const jornada = document.getElementById("jornada").value;
-    Login.signup(
-      nombre,
-      apellido,
-      correo,
-      clave,
-      confirmarClave,
-      grado,
-      jornada
-    );
-  });
-}
+// Sign-up page
+SignupDOM.singup();
