eacc16a96a6a3cefe8aaa6cee12daad62f82450c
Gabriel J. Alvarez C <yogabe123@gmail.com>
2021-02-23 19:51:31 -0500

    se agregó lo relevante a questionForm

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,18 @@
 import "@babel/polyfill";
-import { LoginDOM } from "./homePage/login";
-import { SignupDOM } from "./signupPage/signup";
-
+import { LoginDOM } from "./pages/login";
+import { SignupDOM } from "./pages/signup";
+import { QuestionForm } from "./forms/questionForm";
 // HomePage
 LoginDOM.login();
 LoginDOM.animationInput();
 
 // Sign-up page
-SignupDOM.singup();
+SignupDOM.signup();
+
+// Question Form
+
+if (document.URL === 'http://127.0.0.1:3000/questions/create') {
+    QuestionForm.showPage();
+    QuestionForm.showTopics();
+    QuestionForm.showSubTopics();
+}
